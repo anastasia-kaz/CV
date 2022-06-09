6d6fb9843a55dd963d64cd337b4e42b8e401e5ee
anastasia-kaz <anastasia-kaz@mail.ru>
2022-06-09 20:46:35 +0300

    nav and media

--- main.js
+++ main.js
@@ -1,24 +1,15 @@
-(function (){
-    const burgerItem = document.querySelector('.burger')
-    const menu = document.querySelector('.navigation')
-    const menuCloseItem = document.querySelector('.nav_menu_closed')
-    const links = document.querySelectorAll('.nav_item')
+const burgerBtn = document.querySelector('.burger_btn')
+const menu = document.querySelector('.navigation')
+const links = document.querySelectorAll('.nav_item')
 
-    burgerItem.addEventListener('click', ()=>{
-        menu.classList.add('header_nav_active')
-        menu.classList.add('nav_menu_closed')
+burgerBtn.addEventListener('click', () => {
+    burgerBtn.classList.toggle('open')
+    menu.classList.toggle('header_nav_active')
+})
 
-    });
 
-    menuCloseItem.addEventListener('click', ()=>{
+for (let link of links) {
+    link.addEventListener('click', () => {
         menu.classList.remove('header_nav_active')
     });
-
-
-    for (let link of links) {
-        link.addEventListener('click', ()=>{
-            menu.classList.remove('header_nav_active')
-        });
-    }
-
-}());
+}
